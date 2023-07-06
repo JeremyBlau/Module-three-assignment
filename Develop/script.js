62be9c63ee8c8b9e3ab5a96b52315fadefb97591
jwblau2 <jeremy@abundantly.com>
2023-07-06 11:08:23 -0600

    simplifying the code for readability without sacrificting function

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,34 @@
 // Assignment code here
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChars = "0123456789";
+const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
 // Function to generate a password based on user criteria
 function generatePassword() {
-  var passwordLength = prompt("Enter the desired length of your password (between 8 and 128 characters):");
+  let passwordLength = prompt("Enter the desired length of your password (between 8 and 128 characters):");
   passwordLength = parseInt(passwordLength);
 
   // Validate the password length
   if (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength)) {
     alert("Invalid password length. Please enter a number between 8 and 128.");
-    return "Click on the Generate Password Button to attempt the password generation again.";
+    return "Click on the Generate Password button to attempt the password generation again.";
   }
 
   // Prompt for character types to include
-  var includeLowercase = prompt("Include lowercase characters? (Yes/No)").toLowerCase() === "yes";
-  var includeUppercase = prompt("Include uppercase characters? (Yes/No)").toLowerCase() === "yes";
-  var includeNumeric = prompt("Include numeric characters? (Yes/No)").toLowerCase() === "yes";
-  var includeSpecial = prompt("Include special characters? (Yes/No)").toLowerCase() === "yes";
+  const includeLowercase = prompt("Do you wish to include lowercase characters? (Yes/No)").toLowerCase() === "yes";
+  const includeUppercase = prompt("Do you wish to include uppercase characters? (Yes/No)").toLowerCase() === "yes";
+  const includeNumeric = prompt("Do you wish to include numeric characters? (Yes/No)").toLowerCase() === "yes";
+  const includeSpecial = prompt("Do you wish to include special characters? (Yes/No)").toLowerCase() === "yes";
 
-  // Validate at least one character type is selected
+  // Validate at least one character type has been selected by the user selected
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-    alert("You have to select Yes for at leaset one of the the following character types: Lowercase, Uppercase, Numerics and/or Special Characters.");
-    return "Click on the Generate Password Button to attempt the password generation again.";
+    alert("You have to input Yes for at leaset one the prompts about including a Lowercase, Uppercase, Numerics and/or Special Characters.");
+    return "Click on the Generate Password button to attempt the password generation again.";
   }
 
   // Generate the password based on selected criteria
-  var availableChars = "";
+  let availableChars = "";
   if (includeLowercase) {
     availableChars += lowercaseChars;
   }
